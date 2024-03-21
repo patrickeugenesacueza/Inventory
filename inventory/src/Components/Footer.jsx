6395fc858118589020f7f5c538fa631276b20612
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-03-22 01:00:26 +0800

    Update: Login.jsx

---
+++ inventory/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Footer(){
+    return(
+        <>
+            <footer className="bg-[#06606f] h-[20vh] w-full rounded-md">
+            </footer>
+        </>
+    )
+}
--- inventory/src/Pages/Login.jsx
+++ inventory/src/Pages/Login.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import NavLogin from "../Components/NavLogin";
 import Nav from "../Components/Nav";
+import Footer from "../Components/Footer";
 export default function Login(){
     return(
         <>
-            <div>
+            <div className="px-[.5em] md:px-[1em] pb-[.5em]  md:pb-[1.5em] ">
                 <NavLogin/>
+                <div className="h-[100vh] w-full">
+                    <span className="text-center">Log in to Y-Inventory</span>
+                </div>
+                <Footer/>
             </div>
         </>
     )
